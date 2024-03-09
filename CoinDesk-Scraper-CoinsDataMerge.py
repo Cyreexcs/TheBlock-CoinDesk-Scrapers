1b73fb5f29924a3655522862f07ec80ef65a6907
Slim Ben Hamouda <70235827+Cyreexcs@users.noreply.github.com>
2024-03-09 13:12:56 +0100

    Removed-Comments

--- CoinDesk-Scraper-CoinsDataMerge.py
+++ CoinDesk-Scraper-CoinsDataMerge.py
@@ -16,22 +16,18 @@ def scrape_latest_articles():
     # URL of the CoinDesk website
     url = 'https://www.coindesk.com/'
 
-    # Send a GET request to the URL
     response = requests.get(url)
 
     # Check if the request was successful
     if response.status_code == 200:
-        # Parse the HTML content of the page
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        # Find all elements representing articles
         articles = soup.find_all('div', class_='static-cardstyles__StaticCardWrapper-sc-1kiw3u-0 iiwocm') + \
                    soup.find_all('div', class_='side-cover-cardstyles__SideCoverCardData-sc-1nd3s5z-2 gnuOAQ')
 
         # Dictionary to store the titles and post times of the last 5 articles
         latest_articles_dict = {}
 
-        # Extract the titles and post times of the last 5 articles
         for index, article in enumerate(articles[:7], start=1):
             title_elem = article.find('h2', class_='typography__StyledTypography-sc-owin6q-0 dtjHgI')
             title = title_elem.text.strip() if title_elem else 'Unknown'
@@ -46,7 +42,6 @@ def scrape_latest_articles():
         return {}
 
 
-# Function to fetch prices of cryptocurrencies
 def fetch_prices(coins):
     url = f'https://api.coingecko.com/api/v3/simple/price?ids={",".join(coins)}&vs_currencies=usd&include_24hr_change=true&include_market_cap=true'
 
@@ -60,9 +55,7 @@ def fetch_prices(coins):
         return None
 
 
-# Function to analyze sentiment of news articles
 def analyze_sentiment(text):
-    # Use TextBlob for sentiment analysis
     blob = TextBlob(text)
     sentiment_score = blob.sentiment.polarity
     if sentiment_score > 0:
@@ -73,11 +66,9 @@ def analyze_sentiment(text):
         return "Neutral"
 
 
-# Initialize the latest articles dictionary
 latest_articles = {}
 
 
-# Function to merge and print the latest articles
 def merge_and_print(last_output):
     coins = ['dogecoin', 'dogwifcoin', 'pepe', 'shiba-inu', 'mog-coin', 'meme', 'bonk', 'floki', 'myro', 'maga',
              'bitcoin', 'ethereum', 'chainlink', 'binancecoin', 'solana', 'ripple', 'cardano', 'avalanche-2', 'tether',
@@ -85,12 +76,9 @@ def merge_and_print(last_output):
              'bitcoin-cash', 'aptos', 'optimism', 'bittensor', 'render-token', 'injective-protocol', 'kaspa',
              'arbitrum', 'celestia', 'fetch-ai']
 
-    # Scrape the latest articles
     new_articles = scrape_latest_articles()
 
-    # Check if new articles are found
     if new_articles:
-        # Check if new articles are different from the last output
         if new_articles != last_output:
             prices_data = fetch_prices(coins)
 
@@ -120,19 +108,18 @@ def merge_and_print(last_output):
             else:
                 print("Failed to fetch cryptocurrency prices.")
 
-            return new_articles  # Return the updated container
+            return new_articles 
         else:
             print("No new articles found.")
-            return last_output  # Return the unchanged container
+            return last_output  
     else:
         print("No new articles found.")
-        return last_output  # Return the unchanged container
+        return last_output 
 
 
 # Initially print the latest articles
 latest_articles = merge_and_print(None)
 
-# Start listening for updates
 while True:
     time.sleep(62)
     latest_articles = merge_and_print(latest_articles)
