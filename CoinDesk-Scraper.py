e0d7651597f37ff2915fe367e7f58f00d5cec2d0
Slim Ben Hamouda <70235827+Cyreexcs@users.noreply.github.com>
2024-03-09 13:11:35 +0100

    Update CoinDesk-Scraper.py

--- CoinDesk-Scraper.py
+++ CoinDesk-Scraper.py
@@ -4,13 +4,10 @@ import time
 import requests
 import time
 
-# Replace 'YOUR_API_KEY' with your actual API key
 API_KEY = 'YOUR_API_KEY'
 
-# Your custom coin ID
 your_coin_id = 'your-coin-id'
 
-# List of coins to fetch prices for
 coins = ['dogecoin', 'dogwifcoin', 'pepe', 'shiba-inu', 'mog-coin', 'meme', 'bonk', 'floki', 'myro', 'maga', 'bitcoin', 'ethereum', 'chainlink', 'binancecoin', 'solana','ripple','cardano','avalanche-2','tether','uniswap',
          'bitcoin-cash','near','aptos','optimism','bittensor','render-token','injective-protocol','kaspa','arbitrum','celestia','fetch-ai']
 
@@ -22,7 +19,7 @@ def fetch_prices(coins):
 
     try:
         response = requests.get(url)
-        response.raise_for_status()  # Raise an exception for 4XX and 5XX status codes
+        response.raise_for_status()  
         data = response.json()
         for coin_id, info in data.items():
             if 'usd' in info:
@@ -65,27 +62,20 @@ def parse_time(time_str):
         print(f"Error: Unable to parse time string: {time_str}")
         return None
 
-# Function to scrape the latest articles and return their titles and post times in a dictionary
 def scrape_latest_articles():
-    # URL of the CoinDesk website
     url = 'https://www.coindesk.com/'
 
     # Send a GET request to the URL
     response = requests.get(url)
 
-    # Check if the request was successful
     if response.status_code == 200:
-        # Parse the HTML content of the page
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        # Find all elements representing articles
         articles = soup.find_all('div', class_='static-cardstyles__StaticCardWrapper-sc-1kiw3u-0 iiwocm') + \
                    soup.find_all('div', class_='side-cover-cardstyles__SideCoverCardData-sc-1nd3s5z-2 gnuOAQ')
 
-        # Dictionary to store the titles and post times of the last 5 articles
         latest_articles_dict = {}
 
-        # Extract the titles and post times of the last 5 articles
         for index, article in enumerate(articles[:7], start=1):
             title_elem = article.find('h2', class_='typography__StyledTypography-sc-owin6q-0 dtjHgI')
             title = title_elem.text.strip() if title_elem else 'Unknown'
@@ -107,7 +97,7 @@ def print_latest_articles_with_updates():
         post_time = article_info['post_time']
         print(f"{index}. {title}, Post Time: {post_time}")
     while True:
-        time.sleep(120)  # Wait for 1 second before checking for new articles
+        time.sleep(120)  
         new_articles = scrape_latest_articles()
         if new_articles != old_articles:
             print("New article(s) detected!")
@@ -120,5 +110,4 @@ def print_latest_articles_with_updates():
         else:
             print("No new articles found. Continuing...")
 
-# Start printing the latest articles with updates
 print_latest_articles_with_updates()
